9b0cd2b438ff10924c5d900b5357756edcb059b6
Joshua Hegedus <josh.hegedus@outlook.com>
2023-11-11 12:15:25 +0100

    class 7

--- 2023_autumn/class-7/main.py
+++ 2023_autumn/class-7/main.py
@@ -1,6 +1,7 @@
 
 # install with py -m pip install pendulum
 import pendulum as p
+from mypackage import base as mp
 
 def time_zones():
     cities = [
@@ -19,4 +20,20 @@ def time_zones():
     for i in range(len(times)):
         print(f'{cities[i]}: {times[i]}')
 
+
 time_zones()
+
+
+def using_mypackage():
+    num_1 = 33
+    num_2 = 24
+
+    avg = mp.average(num_1, num_2)
+    print(avg)
+    po = mp.power(num_1, num_2)
+    print(po)
+    print(mp.prime_finder(20))
+    print(mp.matrix_generator())
+
+
+using_mypackage()

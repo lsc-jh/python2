f8c0e4444fd6403b71866a51467c552eb6faa3c3
Joshua Hegedus <josh.hegedus@outlook.com>
2023-12-15 18:39:32 +0100

    added more tasks

--- 2023_autumn/class-12/main.py
+++ 2023_autumn/class-12/main.py
@@ -18,6 +18,27 @@ def openColumns():
     avg = summ / rows
     print(summ)
     print(f"Az atlag kor: {avg}")
+# openColumns()
 
 
-openColumns()
+def oldestWinner():
+    csvfile = pd.read_csv("oscar_age_female.csv", usecols=["Age", "Name"])
+    max_age = max(csvfile["Age"])
+    myRow = csvfile[csvfile["Age"] == max_age]
+    print("The oldest winner is " + str(myRow.iloc[0, 0]) + " years old and she is" + str(myRow.iloc[0, 1]).replace("\"",""))
+# oldestWinner()
+
+
+def fargoWin():
+    csvfile = pd.read_csv("oscar_age_female.csv", usecols=["Year", "Movie"], skipinitialspace=True)
+    myRow = csvfile[csvfile["Movie"] == "Fargo"]
+    print(str(myRow.iloc[0, 1]) + " won the Oscar in " + str(myRow.iloc[0, 0]))
+# fargoWin()
+
+
+def dataRow(row):
+    csvfile = pd.read_csv("oscar_age_female.csv")
+    myRow = csvfile.iloc[row]
+    print(myRow)
+# dataRow(5)
+

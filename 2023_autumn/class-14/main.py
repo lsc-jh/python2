e2fe69f1e1293b9c966f701989e361e7fb103627
Joshua Hegedus <josh.hegedus@outlook.com>
2024-01-12 18:53:04 +0100

    class 14 progress

--- 2023_autumn/class-14/main.py
+++ 2023_autumn/class-14/main.py
@@ -6,8 +6,24 @@ HEIGHT = 500
 SIZE = (WIDTH, HEIGHT)
 WINDOW = pygame.display.set_mode(SIZE)
 
-# Colors
+SPACESHIP_WIDTH = 80
+SPACESHIP_HEIGHT = 60
+VEL = 5
 
+YELLOW_S_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+RED_S_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+
+RED_S_START = pygame.transform.rotate(
+    pygame.transform.scale(RED_S_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),
+    270
+)
+
+YELLOW_S_START = pygame.transform.rotate(
+    pygame.transform.scale(YELLOW_S_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),
+    90
+) 
+
+# Colors
 WHITE = (255, 255, 255)
 
 def drawWindow(red, yellow):

b37e5f59805cf8a7fe9f7b12f988a7ccc3245e42
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-22 18:23:48 +0100

    asd

--- 2024_spring/class-6/tictactoe.py
+++ 2024_spring/class-6/tictactoe.py
@@ -6,11 +6,28 @@ class Player:
 
 
 class Option:
-    def __init__(self, name):
+    def __init__(self, name, value):
         self.name = name
-        self.value = 0
+        self.value = value
 
 
 options = [
-    Option
+    Option("Rock", "rock"),
+    Option("Paper", "paper"),
+    Option("Scissors", "scissors")
 ]
+
+def option_hits(option1, option2):
+    if option1 == option2:
+        return None
+
+    if option1 == "rock" and option2 == "scissors":
+        return True
+
+    if option1 == "scissors" and option2 == "paper":
+        return True
+    
+    if option1 == "paper" and option2 == "rock":
+        return True
+
+    return False

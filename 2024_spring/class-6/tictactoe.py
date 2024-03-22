9395f64690c849c368fcf94e4339493c5a4c5b90
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-22 18:20:12 +0100

    added game

---
+++ 2024_spring/class-6/tictactoe.py
@@ -0,0 +1,16 @@
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+
+class Option:
+    def __init__(self, name):
+        self.name = name
+        self.value = 0
+
+
+options = [
+    Option
+]

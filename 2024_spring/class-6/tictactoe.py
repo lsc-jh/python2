ca5cd944ecdd7d9b2027433e57b244bea96f273e
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-22 18:49:27 +0100

    finished class 6

--- 2024_spring/class-6/tictactoe.py
+++ 2024_spring/class-6/tictactoe.py
@@ -12,6 +12,9 @@ class Option:
         self.name = name
         self.value = value
 
+    def __str__(self):
+        return self.name
+
 
 options = [
     Option("Rock", "rock"),
@@ -35,7 +38,15 @@ def option_hits(option1, option2):
     return False
 
 def play(player1, player2):
-    pass
+    active_player = player1
+    while True:
+        menu = SingleMenu("Select an option", options)
+        player1_selection = menu.show()
+        player2_selection = menu.show()
+        print(f"{player1.name} selected {player1_selection}")
+        print(f"{player2.name} selected {player2_selection}")
+        input("Press Enter to continue...")
+
 
 def main():
     print("Welcom to the Rock, Paper, Scissors game!")
